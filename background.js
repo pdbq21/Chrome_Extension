011338a3cd98240005a3540b27b2b580ba975645
pdbq21 <pdbq2122@gmail.com>
2017-12-05 17:38:19 +0200

    need: change height and add first page with types

--- background.js
+++ background.js
@@ -1,9 +1,77 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  var action_url = "javascript:window.print();";
-  chrome.tabs.update(tab.id, {url: action_url});
+ // var bkg = chrome.extension.getBackgroundPage();
+
+  chrome.tabs.executeScript(tab.id, {file: "script.js"}, function() {
+    // Note: we also sent a message above, upon loading the event page,
+    // but the content script will not be loaded at that point, so we send
+    // another here.
+    //bkg.console.log(1)
+    var action_url = "javascript:window.print();";
+    chrome.tabs.update(tab.id, {url: action_url});
+  });
+
 });
+
+
+/*
+
+
+function getCurrentTabUrl(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, (tabs) => {
+    var tab = tabs[0];
+    var url = tab.url;
+    console.assert(typeof url === 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+
+  // Most methods of the Chrome extension APIs are asynchronous. This means that
+  // you CANNOT do something like this:
+  //
+  // var url;
+  // chrome.tabs.query(queryInfo, (tabs) => {
+  //   url = tabs[0].url;
+  // });
+  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+}
+
+
+function changeBackgroundColor(color) {
+  var script = 'document.body.style.backgroundColor="' + color + '";';
+  // See https://developer.chrome.com/extensions/tabs#method-executeScript.
+  chrome.tabs.executeScript({
+    code: script
+  });
+}
+
+
+function getSavedBackgroundColor(url, callback) {
+  // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
+  // for chrome.runtime.lastError to ensure correctness even when the API call
+  // fails.
+  chrome.storage.sync.get(url, (items) => {
+    callback(chrome.runtime.lastError ? null : items[url]);
+  });
+}
+
+function saveBackgroundColor(url, color) {
+  var items = {};
+  items[url] = color;
+  // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
+  // optional callback since we don't need to perform any action once the
+  // background color is saved.
+  chrome.storage.sync.set(items);
+}
+
+
+*/
+
+
